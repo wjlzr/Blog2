f1e1650fbbc3f096b493fe596e66c0b7d535bec3
wjlzr <13773652841@163.com>
2018-03-18 22:31:34 +0800

    博客

---
+++ application/views/Home/index.php
@@ -0,0 +1,30 @@
+<?php
+	echo
+<<< KANEDO
+<!--
+
+
+
+
+
+		 |                             |        
+		 |  /   _` |  __ \    _ \   _` |   _ \  
+		   <   (   |  |   |   __/  (   |  (   | 
+		_|\_\ \__,_| _|  _| \___| \__,_| \___/  
+
+
+
+
+
+
+
+-->
+KANEDO;
+	$this->load->view('Home/inc/frame');
+	$this->load->view('Home/inc/right');
+	$this->load->view('Home/inc/logo');
+	$this->load->view('Home/inc/image');
+	$this->load->view('Home/content');
+	$this->load->view('Home/inc/footer');
+	$this->load->view('Home/inc/loadScripts');
+?>
